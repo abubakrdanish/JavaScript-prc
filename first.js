11d5a78efcf7d75ba1490e359a38cdb1c25d0d01
Abubakar Bin Danish <55455591+abubakrdanish@users.noreply.github.com>
2024-07-03 19:04:02 +0500

    Add files via upload

---
+++ first.js
@@ -0,0 +1,7 @@
+// let h2 = document.querySelector("h2");
+// console.dir(h2.innerText)
+// h2.innerText = h2.innerText + " from Apna college"
+
+let divs =document.querySelectorAll(".box");
+console.log(divs[0]);
+divs[0].innerText = "new unique value 1";
